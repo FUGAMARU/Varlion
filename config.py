ee1e3ec5abd08099553f1244fb1eb21fc2d20941
FUGAMARU <gofugashi@gmail.com>
2022-11-09 11:23:22 +0900

    [Update] 設定ファイルをJsonからPythonスクリプトに変更

---
+++ config.py
@@ -0,0 +1,23 @@
+import pytz
+
+# Authentication
+ACCOUNT_NUMBER = 1234567
+PASSWORD = "password"
+SERVER = "MT5-Server-01"
+
+# Order
+FILLING_TYPE = "FOK"
+LOT = 0.01
+STOPLOSS = 100
+
+# Parameters
+SINCE = 5
+UNTIL = 10
+SHORT_SMA = 5
+LONG_SMA = 10
+
+# General
+SERVER_TIMEZONE = pytz.timezone("America/New_York")
+USE_EXTERNAL_SOURCE = True
+SOUCE_URL = ""
+LINE_PREFIX = ""
